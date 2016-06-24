374f98eeea84b2d256dc4411c3a4d4e235bde22c
Ralf Kotulla <kotulla@wisc.edu>
2016-06-24 09:45:44 -0500

    Changed the way the weight map is specified: Now via -w/--weight=weight.fits. all other parameters are now taken to be input files to be converted to png files.

--- makequickview.py
+++ makequickview.py
@@ -7,58 +7,68 @@ import os, sys
 
 from PIL import Image, ImageFont, ImageDraw
 #import Image
+from optparse import OptionParser
 
 print Image.SAVE
 
 if __name__ == "__main__":
 
-    fn = sys.argv[1]
-    weight_fn = sys.argv[2]
+    parser = OptionParser()
+    parser.add_option("-w", "--weight", dest="weight_fn",
+                      help="weight filename",
+                      default="", type=str)
+    (options, cmdline_args) = parser.parse_args()
 
-    #
-    # Open input files
-    #
-    hdulist = fits.open(fn)
-    data = hdulist[0].data
+    if (os.path.isfile(options.weight_fn)):
+        weight_hdulist = fits.open(options.weight_fn)
+        weight_map = weight_hdulist[0].data
+    else:
+        weight_map = 1
 
-    weight_hdulist = fits.open(weight_fn)
-    weight_map = weight_hdulist[0].data
+    print cmdline_args
 
-    #
-    # Mask out all areas ouside the covered f.o.v.
-    #
-    data[weight_map <= 0] = numpy.NaN
-    valid_data = data[weight_map > 0]
+    for fn in cmdline_args:
+        #
+        # Open input files
+        #
+        hdulist = fits.open(fn)
+        data = hdulist[0].data
 
-    good_data = weight_map > 0
+        #
+        # Mask out all areas ouside the covered f.o.v.
+        #
+        data[weight_map <= 0] = numpy.NaN
+        valid_data = data[weight_map > 0]
 
-    for iteration in range(3):
+        good_data = weight_map > 0
 
-        qs = numpy.nanpercentile(data[good_data], q=[16,50,84])
+        for iteration in range(3):
 
-        _median = qs[1]
-        _sigma = (qs[2] - qs[0]) / 2.
-        _mingood = _median - 3*_sigma
-        _maxgood = _median + 3*_sigma
-        good_data[ (data<_mingood) | (data>_maxgood)] = False
+            qs = numpy.nanpercentile(data[good_data], q=[16,50,84])
 
-        print iteration, _median, _sigma, _mingood, _maxgood
+            _median = qs[1]
+            _sigma = (qs[2] - qs[0]) / 2.
+            _mingood = _median - 3*_sigma
+            _maxgood = _median + 3*_sigma
+            good_data[ (data<_mingood) | (data>_maxgood)] = False
 
-    #
-    # Good cuts are from -5sigma - 5*sigma
-    #
-    min_level = _median - 5*_sigma
-    max_level = _median + 5*_sigma
+            print iteration, _median, _sigma, _mingood, _maxgood
 
-    greyscale = (data - min_level) / (max_level - min_level)
-    greyscale[greyscale < 0] = 0
-    greyscale[greyscale >= 1] = 1
+        #
+        # Good cuts are from -5sigma - 5*sigma
+        #
+        min_level = _median - 5*_sigma
+        max_level = _median + 5*_sigma
 
-    output_fn = fn[:-5]+".jpg"
-    print output_fn
-    image = Image.fromarray(numpy.uint8(greyscale * 255))
-    image = image.transpose(Image.FLIP_TOP_BOTTOM)
-    image.save(output_fn, "PNG")
-    del image
+        greyscale = (data - min_level) / (max_level - min_level)
+        greyscale[greyscale < 0] = 0
+        greyscale[greyscale >= 1] = 1
+
+        output_fn = fn[:-5]+".png"
+        print output_fn
+        image = Image.fromarray(numpy.uint8(greyscale * 255))
+        image = image.transpose(Image.FLIP_TOP_BOTTOM)
+        image.save(output_fn, "PNG")
+        del image
 
 
