0ad4757ada52c95810dbb9a58added2dcde60595
Ralf Kotulla <ralf.kotulla@gmail.com>
2016-07-08 10:34:02 -0500

    minor mods to fix issues found by phpstorm

--- php/index.php
+++ php/index.php
@@ -91,7 +91,6 @@ if (is_file($comment_fn)) {
     </div>
 </div>
 
-</div>
 <h1><?= $objname ?></h1>
 <div class="comments">
     <form method="POST" action="../save_comment.php" target="_self">
@@ -100,7 +99,7 @@ if (is_file($comment_fn)) {
         <input name="objname" type="hidden" value="<?=$objname?>">
     </form>
 </div>
-<p><ul>
+<ul>
     <li><a href="https://ned.ipac.caltech.edu/cgi-bin/objsearch?objname=<?= $objname ?>&extend=no&hconst=73&omegam=0.27&omegav=0.73&corr_z=1&out_csys=Equatorial&out_equinox=J2000.0&obj_sort=RA+or+Longitude&of=pre_text&zv_breaker=30000.0&list_limit=5&img_stamp=YES">NED page</a></li>
     <li><a href="https://ned.ipac.caltech.edu/cgi-bin/NEDatt?objname=<?= $objname ?>">NED classifications</a></li>
     <li><a href="https://ned.ipac.caltech.edu/cgi-bin/imgdata?objname=<?= $objname ?>&hconst=73.0&omegam=0.27&omegav=0.73&corr_z=1">NED images</a></li>
@@ -109,7 +108,6 @@ if (is_file($comment_fn)) {
     <li><a href="http://hla.stsci.edu/hlaview.html#Inventory|filterText%3D%24filterTypes%3D|query_string=<?=$objname?>&posfilename=&poslocalname=&posfilecount=&listdelimiter=whitespace&listformat=degrees&RA=&Dec=&Radius=0.015000&inst-control=all&inst=ACS&inst=ACSGrism&inst=WFC3&inst=WFPC2&inst=NICMOS&inst=NICGRISM&inst=COS&inst=WFPC2-PC&inst=STIS&inst=FOS&inst=GHRS&imagetype=best&prop_id=&spectral_elt=&proprietary=both&preview=1&output_size=256&cutout_size=12.8|ra=&dec=&sr=&level=&image=&inst=ACS%2CACSGrism%2CWFC3%2CWFPC2%2CNICMOS%2CNICGRISM%2CCOS%2CWFPC2-PC%2CSTIS%2CFOS%2CGHRS&ds=">Search Hubble Legacy Archive</a></li>
     <li><a href="http://adsabs.harvard.edu/cgi-bin/nph-abs_connect?db_key=AST&db_key=PRE&qform=AST&arxiv_sel=astro-ph&arxiv_sel=cond-mat&arxiv_sel=cs&arxiv_sel=gr-qc&arxiv_sel=hep-ex&arxiv_sel=hep-lat&arxiv_sel=hep-ph&arxiv_sel=hep-th&arxiv_sel=math&arxiv_sel=math-ph&arxiv_sel=nlin&arxiv_sel=nucl-ex&arxiv_sel=nucl-th&arxiv_sel=physics&arxiv_sel=quant-ph&arxiv_sel=q-bio&sim_query=YES&ned_query=YES&adsobj_query=YES&aut_logic=OR&obj_logic=OR&author=&object=<?=$objname?>&start_mon=&start_year=&end_mon=&end_year=&ttl_logic=OR&title=&txt_logic=OR&text=&nr_to_return=200&start_nr=1&jou_pick=ALL&ref_stems=&data_and=ALL&group_and=ALL&start_entry_day=&start_entry_mon=&start_entry_year=&end_entry_day=&end_entry_mon=&end_entry_year=&min_score=&sort=SCORE&data_type=SHORT&aut_syn=YES&ttl_syn=YES&txt_syn=YES&aut_wt=1.0&obj_wt=1.0&ttl_wt=0.3&txt_wt=3.0&aut_wgt=YES&obj_wgt=YES&ttl_wgt=YES&txt_wgt=YES&ttl_sco=YES&txt_sco=YES&version=1">ADS Object search</a></li>
 </ul>
-</p>
 
 <div class="filtered">
     <h2><?= $objname ?> - gri stack</h2>
@@ -141,7 +139,7 @@ foreach ($lists as $mode => $croplist) {
 
         <div class="filtered"><h2><?= $objname ?> - <?=$mode?> @ <?=sprintf("%.1f",$sizes[0])?> pixels</h2>
             <p>full frame: <a href='<?= $full_fn ?>'><?= $full_fn ?></a><br>
-                <a href='<?=$cropped_fn?>'><img src='<?=$cropped_fn?>' style='width:500;'</img></a></p></div>
+                <a href='<?=$cropped_fn?>'><img class="filtered" src="<?=$cropped_fn?>"></img></a></p></div>
 
 
         <?php
