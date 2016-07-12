6349c60cfe6aba8e048a9a7103ac4b2f6c851124
Ralf Kotulla <ralf.kotulla@gmail.com>
2016-07-12 13:47:30 -0500

    Added new links to Spitzer & WISE archives.

--- php/index.php
+++ php/index.php
@@ -63,6 +63,22 @@ $comment = "";
 if (is_file($comment_fn)) {
     $comment = file_get_contents($comment_fn);
 }
+
+
+$coord_fn = "coord.txt";
+$ra=0;
+$dec=0;
+$coord_valid=false;
+if (is_file($coord_fn)) {
+    $f = fopen($coord_fn, 'r');
+    $line = fgets($f);
+    $coords = explode(' ', $line);
+    if (count($coords) >= 4) {
+        $ra = $coords[2];
+        $dec = $coords[3];
+        $coord_valid=true;
+    }
+}
 ?>
 
 
@@ -107,6 +123,14 @@ if (is_file($comment_fn)) {
     <li><a href="http://simbad.u-strasbg.fr/simbad/sim-id?Ident=<?= $objname ?>&NbIdent=1&Radius=2&Radius.unit=arcmin&submit=submit+id">SIMBad Identifier query</a></li>
     <li><a href="http://hla.stsci.edu/hlaview.html#Inventory|filterText%3D%24filterTypes%3D|query_string=<?=$objname?>&posfilename=&poslocalname=&posfilecount=&listdelimiter=whitespace&listformat=degrees&RA=&Dec=&Radius=0.015000&inst-control=all&inst=ACS&inst=ACSGrism&inst=WFC3&inst=WFPC2&inst=NICMOS&inst=NICGRISM&inst=COS&inst=WFPC2-PC&inst=STIS&inst=FOS&inst=GHRS&imagetype=best&prop_id=&spectral_elt=&proprietary=both&preview=1&output_size=256&cutout_size=12.8|ra=&dec=&sr=&level=&image=&inst=ACS%2CACSGrism%2CWFC3%2CWFPC2%2CNICMOS%2CNICGRISM%2CCOS%2CWFPC2-PC%2CSTIS%2CFOS%2CGHRS&ds=">Search Hubble Legacy Archive</a></li>
     <li><a href="http://adsabs.harvard.edu/cgi-bin/nph-abs_connect?db_key=AST&db_key=PRE&qform=AST&arxiv_sel=astro-ph&arxiv_sel=cond-mat&arxiv_sel=cs&arxiv_sel=gr-qc&arxiv_sel=hep-ex&arxiv_sel=hep-lat&arxiv_sel=hep-ph&arxiv_sel=hep-th&arxiv_sel=math&arxiv_sel=math-ph&arxiv_sel=nlin&arxiv_sel=nucl-ex&arxiv_sel=nucl-th&arxiv_sel=physics&arxiv_sel=quant-ph&arxiv_sel=q-bio&sim_query=YES&ned_query=YES&adsobj_query=YES&aut_logic=OR&obj_logic=OR&author=&object=<?=$objname?>&start_mon=&start_year=&end_mon=&end_year=&ttl_logic=OR&title=&txt_logic=OR&text=&nr_to_return=200&start_nr=1&jou_pick=ALL&ref_stems=&data_and=ALL&group_and=ALL&start_entry_day=&start_entry_mon=&start_entry_year=&end_entry_day=&end_entry_mon=&end_entry_year=&min_score=&sort=SCORE&data_type=SHORT&aut_syn=YES&ttl_syn=YES&txt_syn=YES&aut_wt=1.0&obj_wt=1.0&ttl_wt=0.3&txt_wt=3.0&aut_wgt=YES&obj_wgt=YES&ttl_wgt=YES&txt_wgt=YES&ttl_sco=YES&txt_sco=YES&version=1">ADS Object search</a></li>
+    <?php
+    if ($coord_valid) { ?>
+        <li><a href="http://sha.ipac.caltech.edu/applications/Spitzer/SHA/#id=SearchByPosition&RequestClass=ServerRequest&DoSearch=true&SearchByPosition.field.radius=0.13888889000000001&UserTargetWorldPt=<?=$ra?>;<?=$dec?>;EQ_J2000&TargetPanel.field.targetName=<?+$objname?>&SimpleTargetPanel.field.resolvedBy=nedthensimbad&MoreOptions.field.prodtype=aor,pbcd&shortDesc=Position&isBookmarkAble=true&isDrillDownRoot=true&isSearchResult=true">Query Spitzer Archive</a> (<a href="http://sha.ipac.caltech.edu/applications/Spitzer/SHA/">Spitzer Archive Search Page</a>)</li>
+        <li><a href="http://irsa.ipac.caltech.edu/applications/wise/#id=Hydra_wise_wise_1&RequestClass=ServerRequest&DoSearch=true&intersect=CENTER&subsize=0.16666666800000002&mcenter=mcen&schema=allwise-multiband&dpLevel=3a&band=1,2,3,4&UserTargetWorldPt=<?=$ra?>;<?=$dec?>;EQ_J2000&TargetPanel.field.targetName=<?=$objname?>&SimpleTargetPanel.field.resolvedBy=nedthensimbad&preliminary_data=no&coaddId=&projectId=wise&searchName=wise_1&shortDesc=Position&isBookmarkAble=true&isDrillDownRoot=true&isSearchResult=true">Query WISE Archive</a> (<a href="http://irsa.ipac.caltech.edu/applications/wise/">WISE Archive Search Page</a>)</li>
+    <?php } else { ?>
+        <li><a href="http://sha.ipac.caltech.edu/applications/Spitzer/SHA/">Spitzer Archive Search Page</a></li>
+        <li><a href="http://irsa.ipac.caltech.edu/applications/wise/">WISE Archive Search Page</a></li>
+    <?php } ?>
 </ul>
 
 <div class="filtered">
